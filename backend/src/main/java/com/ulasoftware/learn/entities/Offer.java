78e7744348d87ef623c99104413187049a4bb862
ulaecio <ulaecio@live.com>
2023-08-27 01:39:37 -0300

    Resource

--- backend/src/main/java/com/ulasoftware/learn/entities/Offer.java
+++ backend/src/main/java/com/ulasoftware/learn/entities/Offer.java
@@ -1,15 +1,17 @@
 package com.ulasoftware.learn.entities;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,16 +22,15 @@ public class Offer {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String edition;
-	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant startMoment;
-
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant endMoment;
 	
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
 	
 	public Offer() {
 	}
@@ -81,6 +82,10 @@ public class Offer {
 
 	public void setCourse(Course course) {
 		this.course = course;
+	}
+
+	public List<Resource> getResources() {
+		return resources;
 	}
 
 	@Override
