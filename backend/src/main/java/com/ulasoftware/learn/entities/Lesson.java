ab03865a42c2d0f5538a81f02358a8c635796d33
ulaecio <ulaecio@live.com>
2023-08-28 19:44:01 -0300

    Lesson, Content, task

---
+++ backend/src/main/java/com/ulasoftware/learn/entities/Lesson.java
@@ -0,0 +1,105 @@
+package com.ulasoftware.learn.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String position;
+
+	@ManyToOne
+	@JoinColumn(name = "section_id")
+	private Section section;
+
+	@ManyToMany
+	@JoinTable(name = "tb_lessons_done",joinColumns = @JoinColumn(name = "lesson_id"), inverseJoinColumns = {
+			@JoinColumn(name = "user_id"), @JoinColumn(name = "offer_id") })
+	private Set<Enrollment> enrollmentDone = new HashSet<>();
+
+	public Lesson() {
+
+	}
+
+	public Lesson(Long id, String title, String position, Section section) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.position = position;
+		this.section = section;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public Section getSection() {
+		return section;
+	}
+
+	public void setSection(Section section) {
+		this.section = section;
+	}
+
+	public Set<Enrollment> getEnrollmentDone() {
+		return enrollmentDone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lesson other = (Lesson) obj;
+		return Objects.equals(id, other.id);
+	}
+}
